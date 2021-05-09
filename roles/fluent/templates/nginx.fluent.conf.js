042e77bb78ef3279f7a4854d631b6302cefc7018
sohwaje <kissenyo@gmail.com>
2021-05-10 08:48:43 +0900

    update

--- roles/fluent/templates/nginx.fluent.conf.js
+++ roles/fluent/templates/nginx.fluent.conf.js
@@ -1,24 +1,22 @@
-# nginx accesslog
-<source>
-   @type tail
-   path /var/log/*_error.log
-   pos_file /var/log/oauth2client/nginx-error.pos
-   tag nginx.error
-   format /^(?<logtime>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) \[(?<log_level>\w+)\] (?<pid>\d+).(?<tid>\d+): (?<message>.*)$/
-   time_key logtime
-   time_format %Y/%m/%d %H:%M:%S
-</source>
+// <source>
+//    @type tail
+//    path /var/log/*_error.log
+//    pos_file /var/log/oauth2client/nginx-error.pos
+//    tag nginx.error
+//    format /^(?<logtime>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) \[(?<log_level>\w+)\] (?<pid>\d+).(?<tid>\d+): (?<message>.*)$/
+//    time_key logtime
+//    time_format %Y/%m/%d %H:%M:%S
+// </source>
 
-# nginx errorlog
 <source>
    @type tail
-   path /var/log/*_access.log
-   pos_file /var/log/oauth2client/nginx-access.pos
+   path /var/log/*.log
+   pos_file /var/log/spring-boot-log.pos
    tag nginx.access
    format nginx
 </source>
 
-<match nginx.error>
+<match springboot.**>
   @type forward
     <server>
       name fluentd-aggregator
@@ -27,11 +25,11 @@
     </server>
 </match>
 
-<match nginx.access>
-  @type forward
-    <server>
-      name fluentd-aggregator
-      host efk-scream.azure.i-screammedia.com
-      port 24224
-    </server>
-</match>
+// <match nginx.access>
+//   @type forward
+//     <server>
+//       name fluentd-aggregator
+//       host efk-scream.azure.i-screammedia.com
+//       port 24224
+//     </server>
+// </match>
