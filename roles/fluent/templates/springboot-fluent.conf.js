afbab9121f278cc45430923a0113c5efa441c660
sohwaje <kissenyo@gmail.com>
2021-05-10 14:08:59 +0900

    update

--- roles/fluent/templates/springboot-fluent.conf.js
+++ roles/fluent/templates/springboot-fluent.conf.js
@@ -2,7 +2,7 @@
   @type               tail
   tag                 springboot.access
   path                /var/log/*.log
-  pos_file            /var/log/log.pos
+  pos_file            /var/log/pos/log.pos
   rotate_wait         5
   read_from_head      false
     <parse>
