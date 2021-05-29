bc8467b6d15721f8def3d86ac2e1c98f6f244cad
sohwaje <kissenyo@gmail.com>
2021-05-29 19:53:33 +0900

    updat

--- roles/fluent/templates/nginx.fluent.conf.js
+++ roles/fluent/templates/nginx.fluent.conf.js
@@ -1,21 +1,22 @@
-// <source>
-//    @type tail
-//    path /var/log/*_error.log
-//    pos_file /var/log/oauth2client/nginx-error.pos
-//    tag nginx.error
-//    format /^(?<logtime>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) \[(?<log_level>\w+)\] (?<pid>\d+).(?<tid>\d+): (?<message>.*)$/
-//    time_key logtime
-//    time_format %Y/%m/%d %H:%M:%S
-// </source>
-
 <source>
-   @type tail
-   path /var/log/*.log
-   tag nginx.access
-   format none
+  @type tail
+  path /var/log/nginx/nginx/*_error.log
+  pos_file /var/log/nginx/nginx-error.pos
+  tag nginx.error
+  format /^(?<logtime>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) \[(?<log_level>\w+)\] (?<pid>\d+).(?<tid>\d+): (?<message>.*)$/
+  time_key logtime
+  time_format %Y/%m/%d %H:%M:%S
 </source>
 
-<match nginx.access>
+ <source>
+  @type tail
+  path /var/log/nginx/*_access.log
+  pos_file /var/log/nginx/nginx-access.pos
+  tag nginx.access
+  format nginx
+</source>
+
+<match nginx.error>
   @type forward
     <server>
       name fluentd-aggregator
@@ -24,11 +25,11 @@
     </server>
 </match>
 
-// <match nginx.access>
-//   @type forward
-//     <server>
-//       name fluentd-aggregator
-//       host efk-scream.azure.i-screammedia.com
-//       port 24224
-//     </server>
-// </match>
+    <match nginx.access>
+  @type forward
+    <server>
+      name fluentd-aggregator
+      host efk-scream.azure.i-screammedia.com
+      port 24224
+    </server>
+</match>
