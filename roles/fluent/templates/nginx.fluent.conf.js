5c73e6c78f7199ec39d1a572594353dc78716f4f
sohwaje <kissenyo@gmail.com>
2021-05-29 20:13:56 +0900

    update

--- roles/fluent/templates/nginx.fluent.conf.js
+++ roles/fluent/templates/nginx.fluent.conf.js
@@ -1,7 +1,7 @@
 <source>
   @type tail
-  path /var/log/nginx/nginx/*_error.log
-  pos_file /var/log/nginx/nginx-error.pos
+  path /var/log/*_error.log
+  pos_file /var/log/nginx-error.pos
   tag nginx.error
   format /^(?<logtime>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) \[(?<log_level>\w+)\] (?<pid>\d+).(?<tid>\d+): (?<message>.*)$/
   time_key logtime
@@ -10,8 +10,8 @@
 
  <source>
   @type tail
-  path /var/log/nginx/*_access.log
-  pos_file /var/log/nginx/nginx-access.pos
+  path /var/log/*_access.log
+  pos_file /var/log/nginx-access.pos
   tag nginx.access
   format nginx
 </source>
